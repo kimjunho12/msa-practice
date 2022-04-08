3535475d8e3226641245270c4dd1df30bc122f4a
98kimjh <98kimjh@gmail.com>
2022-04-08 14:41:16 +0900

    [client] Emaillist Application Server

---
+++ clients/emaillist08/backend/src/main/java/me/kickscar/emaillist/EmaillistApplication.java
@@ -0,0 +1,20 @@
+package me.kickscar.emaillist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class EmaillistApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(EmaillistApplication.class, args);
+	}
+
+//	@LoadBalanced : GW 사용
+	@Bean
+	public RestTemplate restTemplte() {
+		return new RestTemplate();
+	}
+}
